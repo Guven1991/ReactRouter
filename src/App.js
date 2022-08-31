79e71c1bb8e05841da90b258721951b72b062750
mehmet <sayinmehmet47@gmail.com>
2022-08-31 21:30:03 +0200

    fix:outlet add

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
-import React from "react";
-import "./App.css";
-import About from "./components/About";
-import Users from "./components/Users";
-import Home from "./components/Home";
-import User from "./components/User";
-import Error404 from "./components/Error404";
+import React from 'react';
+import './App.css';
+import About from './components/About';
+import Users from './components/Users';
+import Home from './components/Home';
+import User from './components/User';
+import Error404 from './components/Error404';
 
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/users" element={<Users />}>
-            <Route path="/users/:id" element={<User />} />
-          </Route>
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<Error404 />} />
-          
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="users" element={<Users />}>
+          <Route path=":id" element={<User />} />
+        </Route>
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error404 />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,29 +1,30 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link, Outlet } from 'react-router-dom';
 function Users() {
-const [users,setUsers] = useState([]);
-const [isLoading,setIsLoading] = useState(true);
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-useEffect(() => {
-axios('https://jsonplaceholder.typicode.com/users')
-.then(res =>  setUsers(res.data))
-.finally(() => setIsLoading(false));
-},[])
+  useEffect(() => {
+    axios('https://jsonplaceholder.typicode.com/users')
+      .then((res) => setUsers(res.data))
+      .finally(() => setIsLoading(false));
+  }, []);
 
   return (
     <div>
-        <h2>Users</h2>
-        {isLoading && <div>Loading....</div>}
+      <h2>Users</h2>
+      {isLoading && <div>Loading....</div>}
       <ul>
-        {users.map(user =>(
+        {users.map((user) => (
           <>
             <li key={user.id}>
-            <Link to={`/users/${user.id}`}>{user.name}</Link>
-          </li>
+              <Link to={`/users/${user.id}`}>{user.name}</Link>
+            </li>
           </>
         ))}
       </ul>
+      <Outlet />
     </div>
   );
 }
